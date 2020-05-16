0f104ebe3ef03f116b911e8f726caa0015dafa7e
Sheila R Moore <sherexmykes@github.com>
2020-05-15 21:47:46 -0400

    still working on it

--- api/server.js
+++ api/server.js
@@ -1,18 +1,34 @@
 const express = require('express');
+const morgan = require('morgan')
 const cors = require('cors');
 const helmet = require('helmet');
 
-const authenticate = require('../auth/authenticate-middleware.js');
+const { authenticate } = require('../auth/authenticate-middleware.js');
 const authRouter = require('../auth/auth-router.js');
 const jokesRouter = require('../jokes/jokes-router.js');
+const userRouter = require('../users/user-router.js')
+
 
 const server = express();
 
+server.use(morgan("short"))
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
+server.use('/api/users', userRouter)
+
+server.get("/", (req, res, next) => {
+    res.send("<h2>Server Test</h2>")
+  })
+  server.use((req, res, next) => {
+    res.status(404).json({ message: "This error is your fault"})
+  })
+  
+  server.use((err, req, res, next) => {
+    res.status(500).json({ message: "This error is due to the server."})
+  })
 
 module.exports = server;
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,33 @@
 const router = require('express').Router();
 
-router.post('/register', (req, res) => {
-  // implement registration
-});
+router.post("/register", async (req, res, next) => {
+  try {
+    const user = await Users.add(req.body)
 
-router.post('/login', (req, res) => {
-  // implement login
-});
+    res.status(201).json({ message: "User has been registered", user})
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.post("/login", async (req, res, next) => {
+  try {
+    const { username, password } = req.body
+    const user = await Users.findBy({ username }).first()
+    const passwordValid = await bcrypt.compare(password, user.password)
+
+    if (user && passwordValid) {
+      const token = generateToken(user);
+      res.status(200).json({ 
+        message: `Welcome, ${user.username}!`, token})
+    } else {
+      res.status(401).json({ message: "User Credentials Invalid!"})
+    }
+  } catch (error) {
+    next(error)
+  }
+})
 
 module.exports = router;
+
+
